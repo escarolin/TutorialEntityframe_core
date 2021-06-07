d4115db3347aae4532afdcbf0d5b633758dfaaa8
escarolin <escarolin09@hotmail.com>
2021-06-06 22:42:04 -0400

    agrgando contenido a dos clases

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace tutorial__EntityFramework_Core.Models
 {
-    class Student
+    public partial class Student
     {
+        public Student()
+        {
+            StudentCourse = new HashSet<StudentCourse>();
+        }
+
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? StandardId { get; set; }
+
+        public Standard Standard { get; set; }
+        public StudentAddress StudentAddress { get; set; }
+        public ICollection<StudentCourse> StudentCourse { get; set; }
     }
 }
